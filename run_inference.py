98879bdd2244b36e5c3accabe90e05412ab9da15
527718651@qq.com <527718651@qq.com>
2026-07-20 23:21:09 +0800

    init

--- run_inference.py
+++ run_inference.py
@@ -1,8 +1,8 @@
 import argparse
-from do_infer import main as do_infer
+from scripts.TestInfer import main as do_infer
 
 def main():
-    parser = argparse.ArgumentParser(description="Run nnUNetv2 inference")
+    parser = argparse.ArgumentParser(description="Run inference")
     parser.add_argument("--input", type=str, default="/inputs", help="Path to input directory")
     parser.add_argument("--output", type=str, default="/outputs", help="Path to output directory")
     args = parser.parse_args()
